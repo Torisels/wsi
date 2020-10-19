d1ca7175123f5655a133d1eecd5479054b42ed52
Gustaw Daczkowski <01149695@pw.edu.pl>
2020-10-19 22:02:11 +0200

    Reformat code

--- zad1_minimums/main.py
+++ zad1_minimums/main.py
@@ -1,32 +1,42 @@
 import sympy
 from sympy import Matrix
 
-# define starting point
-X0 = 4
-Y0 = 4
+# define starting point as N dimensional vector
+starting_point = [4, 4]
+# compute number of dimensions
+DIM = len(starting_point)
 # define function for evaluation
 FUNC_EXPRESSION = "(1-x)^2 + 100*(y-x*x)^2"
+# Epsilon as an exit condition
+EPSILON = 10 ** -8
+# Maximum number of iterations
+ITER_MAX = 1000
+# Alpha coefficient
+ALPHA = 0.2
+# Destination (we are looking for minimum)
+DEST = 0
 
+lower_bound = DEST - EPSILON
+upper_bound = DEST + EPSILON
 
 x, y = sympy.symbols("x y")
-
 f = sympy.sympify(FUNC_EXPRESSION)
+starting_vector = sympy.Matrix(starting_point)
 
-# r = f.subs({x: 1, y: 1})
-# print(r)
+gradient = Matrix([f.diff(e) for e in [x, y]])  # compute gradient vector
+hessian = Matrix([[f.diff(p1).diff(p2) for p1 in [x, y]] for p2 in [x, y]])  # compute Hessian
+hessian = hessian.inv()  # Invert hessian
 
-starting_vector = sympy.Matrix([X0, Y0])
-gradient = Matrix([f.diff(e) for e in [x, y]])
-hessian = Matrix([[f.diff(p1).diff(p2) for p1 in [x, y]] for p2 in [x, y]])
-hessian = hessian.inv()
+input_values = list()
 
-for i in range(0, 100):
+for i in range(ITER_MAX):
     sub_inp = dict(zip([x, y], starting_vector))
-    starting_vector = starting_vector - 0.2 * hessian.subs(sub_inp) * gradient.subs(sub_inp)
-    print(starting_vector)
+    value_at_point = f.subs(sub_inp).doit()
+    if lower_bound <= value_at_point <= upper_bound:
+        break
+    starting_vector = starting_vector - ALPHA * hessian.subs(sub_inp) * gradient.subs(sub_inp)
+    input_values.append(starting_vector)
 
-print(starting_vector)
+print(input_values)
 
-# sympy.plotting.plot3d(f, (x, -1, 1), (y, -1, 1))
 
-# sympy.plotting.plot3d(F)
